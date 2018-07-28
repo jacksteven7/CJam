b50f760c1d599d11125c7cefa3f96e22624cf65c
Jack Steven Ibarra Marquez <jacksteven7@gmail.com>
2018-07-28 18:06:30 -0500

    update template

--- Template.rb
+++ Template.rb
@@ -15,3 +15,20 @@ file = File.open(filename)
 file.readline
 
 L, D, N = gets.strip.split(' ').map(&:to_i)
+
+
+#!/bin/ruby
+
+filename = ARGV.first
+file = File.open(filename)
+
+L, D, N = file.readline.split(' ').map(&:to_i)
+
+D.times do |i|
+  dic.push file.readline 
+end
+
+output = File.open("answer.out","w")
+output.write("Case ##{i+1}: #{found}\n")
+end
+
